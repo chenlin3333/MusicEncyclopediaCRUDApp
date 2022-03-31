6925463b0db25da1dd71d3493b9fa33a2c8f87a4
chenlin33 <cheny@DESKTOP-2HISQPD>
2022-03-30 21:37:26 -0400

    fix sql syntax errors, script can now successfully create relation tables in postgresql

--- Scripts/createDataBase.js
+++ Scripts/createDataBase.js
@@ -7,68 +7,68 @@ const db = new pg.Pool({
  port: '5432'
 });
 
-try{
-    db.query("CREATE TABLE user" + 
-        "(userID INT PRIMARY KEY," +
-         "userName VARCHAR(26) NOT NULL UNIQUE," +
-         "passWord VARCHAR(26) NOT NULL,)");
+// const variables for all table names
+const user = "users"
+const musicCollection = "musicCollection"
+const composer = "composer"
+const composition = "composition"
+const genre = "genre"
+const userComposer = "userComposer"
+const userComposition = "userComposition"
+const compositionGenre = "compositionGenre"
+const musicCollectionComposition = "musicCollectionComposition"
 
-    db.query("CREATE TABLE musicCollection" + 
-         "(musicCollectionID INT PRIMARY KEY," +
-          "name VARCHAR(26) NOT NULL," +
-          "userID INT," + 
-          "FOREIGN KEY (userID) REFERENCES user (userID),)");
- 
-     db.query("CREATE TABLE composer" + 
-         "(composerID INT PRIMARY KEY," +
-          "biography TEXT NOT NULL," +
-          "name VARCHAR(26) NOT NULL,)");
- 
-     db.query("CREATE TABLE composition" + 
-         "(compositionID INT PRIMARY KEY," +
-          "title VARCHAR(26) NOT NULL UNIQUE," +
-          "releasedDate DATE NOT NULL," +
-          "length FLOAT(4, 2) NOT NULL," +
-          "composerID INT," +
-          "FOREIGN KEY (composerID) REFERENCES composer (composerID)");
- 
-     db.query("CREATE TABLE genre" + 
-         "(genreID INT PRIMARY KEY," +
-          "name VARCHAR(26) NOT NULL UNIQUE,");
- 
-     db.query("CREATE TABLE userComposer" + 
-         "(userComposerID INT PRIMARY KEY," +
-          "userID INT," +
-          "composerID INT," + 
-          "FOREIGN KEY (userID) REFERENCES user (userID)," +
-          "FOREIGN KEY (composerID) REFERENCES composer (composerID)");
- 
-     db.query("CREATE TABLE userComposition" + 
-         "(userCompositionID INT PRIMARY KEY," +
-          "userID INT," +
-          "compositionID INT," + 
-          "FOREIGN KEY (userID) REFERENCES user (userID)," +
-          "FOREIGN KEY (compositionID) REFERENCES composition (compositionID)");
- 
-     db.query("CREATE TABLE compositionGenre" + 
-         "(compositionGenre INT PRIMARY KEY," +
-          "compositionID INT," +
-          "genreID INT," + 
-          "FOREIGN KEY (compositionID) REFERENCES compostion (compositionID)," +
-          "FOREIGN KEY (genreID) REFERENCES genre (genreID)");
- 
-     db.query("CREATE TABLE musicCollectionComposition" + 
-         "(musicCollectionCompositionID INT PRIMARY KEY," +
-          "musicCollectionID INT," +
-          "compositionID INT," + 
-          "FOREIGN KEY (musicCollectionID) REFERENCES musicCollection (musicCollectionID)," +
-          "FOREIGN KEY (compositionID) REFERENCES composition (compositionID)");
-    
+try{
+    db.query(`CREATE TABLE IF NOT EXISTS ${user} 
+        (userID INT PRIMARY KEY,
+         userName VARCHAR(26) NOT NULL UNIQUE,
+         passWord VARCHAR(26) NOT NULL)`);
+
+    db.query(`CREATE TABLE IF NOT EXISTS ${musicCollection}
+        (musicCollectionID INT PRIMARY KEY,
+         name VARCHAR(26) NOT NULL,
+         userID INT REFERENCES ${user} (userID) ON DELETE CASCADE)`);
+
+    db.query(`CREATE TABLE IF NOT EXISTS ${composer} 
+        (composerID INT PRIMARY KEY, 
+         biography TEXT NOT NULL,
+         name VARCHAR(26) NOT NULL UNIQUE)`);
+        
+    db.query(`CREATE TABLE IF NOT EXISTS ${composition}
+        (compositionID INT PRIMARY KEY,
+         title VARCHAR(26) NOT NULL UNIQUE,
+         releasedDate DATE NOT NULL,
+         length FLOAT(4) NOT NULL,
+         composerID INT REFERENCES ${composer} (composerID) ON DELETE CASCADE)`);
+
+    db.query(`CREATE TABLE IF NOT EXISTS ${genre}
+        (genreID INT PRIMARY KEY,
+         name VARCHAR(26) NOT NULL UNIQUE)`);
+
+    db.query(`CREATE TABLE IF NOT EXISTS ${userComposer} 
+        (userComposerID INT PRIMARY KEY,
+         userID INT REFERENCES ${user} (userID) ON DELETE CASCADE,
+         composerID INT REFERENCES ${composer} (composerID) ON DELETE CASCADE)`);
+   
+    db.query(`CREATE TABLE IF NOT EXISTS ${userComposition}
+        (userCompositionID INT PRIMARY KEY,
+         userID INT REFERENCES ${user} (userID) ON DELETE CASCADE,
+         compositionID INT REFERENCES ${composition} (compositionID) ON DELETE CASCADE)`);
+
+    db.query(`CREATE TABLE IF NOT EXISTS ${compositionGenre}
+        (compositionGenre INT PRIMARY KEY,
+         compositionID INT REFERENCES ${composition} (compositionID) ON DELETE CASCADE,
+         genreID INT REFERENCES ${genre} (genreID) ON DELETE CASCADE)`);
+   
+    db.query(`CREATE TABLE IF NOT EXISTS ${musicCollectionComposition}
+        (musicCollectionCompositionID INT PRIMARY KEY,
+         musicCollectionID INT REFERENCES ${musicCollection} (musicCollectionID) ON DELETE CASCADE,
+         compositionID INT REFERENCES ${composition} (compositionID) ON DELETE CASCADE)`);
+
     db.end();
 }
 catch (err){
     console.error(err);
     db.end();
 }
-
 
