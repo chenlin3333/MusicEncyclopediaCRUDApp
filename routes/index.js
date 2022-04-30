7471aa86fd84b4e22d0fb4ee85841daace0539ab
chenlin33 <cheny@DESKTOP-2HISQPD>
2022-04-30 17:22:40 -0400

    add functionality for user to create a new composer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../public/javascripts/db');
+const pgClient = require('../public/javascripts/db').db;
+var currentUser;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,14 +11,14 @@ router.get('/', function(req, res, next) {
 router.post('/', async function(req, res, next) {
   var userName = req.body.Username;
   var passWord = req.body.Password;
-  pgClient = db.db;
   var result = await pgClient.query(`
     select count(*)
     from users
     where username = '${userName}' and password = '${passWord}'`);
 
   if(result[0].count == '1'){
-    res.render('userpage', {user: `Logged in as ${userName}`});
+    currentUser = userName
+    res.render('userpage', {user: `Logged in as ${currentUser}`});
   }
   else{
     res.render('index', {description: 'Incorrect Password or Username. Try Again.'});
@@ -32,7 +33,6 @@ router.get('/register', function(req, res, next) {
 router.post('/register', async function(req, res, next) {
   var userName = req.body.Username;
   var passWord = req.body.Password;
-  pgClient = db.db;
   var result = await pgClient.query(`
     select count(*)
     from users
@@ -57,7 +57,45 @@ router.post('/register', async function(req, res, next) {
 });
 
 router.get('/composer', function(req, res, next) {
-  res.render('composer', { title: 'Please create an username and password.'});
+  res.render('composer', {});
 });
 
+router.post('/composer', async function(req, res, next) {
+  var name = req.body.name;
+  var bio = req.body.bio;
+  var result = await pgClient.query(`
+    select count(*)
+    from composer
+    where name = '${name}'`);
+
+  if(result[0].count == '0'){
+    var latestId = await pgClient.query(`
+      select composerid
+      from composer
+      order by composerid desc
+      limit 1`);
+    
+    var nextId = latestId[0].composerid + 1;
+    await pgClient.query(`
+      insert into composer(composerid, biography, name)
+      values(${nextId}, '${bio}', '${name}')`);
+
+    res.render('composer', { result: 'Composer Created!'});
+  }
+  else{
+    res.render('composer', {result: 'This Composer already exists!'});
+  }
+  
+});
+
+router.post('/createCollection', function(req, res, next) {
+  var name = req.body.name;
+  res.render('userpage', { user: `Logged in as ${currentUser}`, creationResult: 'success!'});
+});
+
+
+
+router.get('/userpage', function(req, res, next) {
+  res.render('userpage', { user: `Logged in as ${currentUser}`});
+});
 module.exports = router;
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/register', function(req, res, next) {
-  res.render('index', { title: 'Music Encyclopedia'});
-  //res.render('register', { title: 'WORKING'});
-});
-
-module.exports = router;
